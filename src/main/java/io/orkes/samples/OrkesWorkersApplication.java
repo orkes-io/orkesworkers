bc71eb099c99e5b5d9e2bebd093ddbdcb2838a96
nohup23 <jeu@orkes.io>
2021-10-11 22:29:48 -0700

    Initial Commit

---
+++ src/main/java/io/orkes/samples/OrkesWorkersApplication.java
@@ -0,0 +1,13 @@
+package io.orkes.samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrkesWorkersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrkesWorkersApplication.class, args);
+	}
+
+}
