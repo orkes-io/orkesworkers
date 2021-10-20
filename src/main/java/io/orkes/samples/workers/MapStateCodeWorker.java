d242dc5be0fc023d8812be5c0cad3d8203ba2ba5
nohup23 <jeu@orkes.io>
2021-10-19 23:43:01 -0700

    Add the MapStateCodeWorker for samples

--- src/main/java/io/orkes/samples/workers/MapStateCodeWorker.java
+++ src/main/java/io/orkes/samples/workers/MapStateCodeWorker.java
@@ -3,9 +3,6 @@ package io.orkes.samples.workers;
 import com.netflix.conductor.client.worker.Worker;
 import com.netflix.conductor.common.metadata.tasks.Task;
 import com.netflix.conductor.common.metadata.tasks.TaskResult;
-import org.springframework.boot.json.JsonParseException;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
@@ -20,14 +17,22 @@ public class MapStateCodeWorker implements Worker {
         return "map_state_codes";
     }
 
+
     @Override
     public TaskResult execute(Task task) {
-        List<Map<String, Object>> data = (List<Map<String, Object>>) task.getInputData().get("data");
+        List<Map<String, Object>> statePopulations = (List<Map<String, Object>>) task.getInputData().get("statePopulations");
+
+        statePopulations.forEach(stateData -> {
+            String state = (String) stateData.get("State");
+            stateData.put("stateCode", Constants.STATE_CODES.getOrDefault(state, "NOT_AVAILABLE"));
+        });
+
         TaskResult result = new TaskResult(task);
-        result.setStatus(TaskResult.Status.COMPLETED);
         String currentTimeOnServer = Instant.now().toString();
         result.log("Mapped states to codes at " + currentTimeOnServer);
-        result.addOutputData("statePopulations", data);
+
+        result.addOutputData("statePopulations", statePopulations);
+        result.setStatus(TaskResult.Status.COMPLETED);
         return result;
     }
 
