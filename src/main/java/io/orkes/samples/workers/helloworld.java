208e61c0ae4528e5b6cb279792262a0b3ff7c990
Doug Sillars <dougsillars@Dougs-MacBook-Air.local>
2022-02-25 15:57:49 -0500

    added header code for cloud

---
+++ src/main/java/io/orkes/samples/workers/helloworld.java
@@ -0,0 +1,21 @@
+package io.orkes.samples.workers;
+import com.netflix.conductor.client.worker.Worker;
+import com.netflix.conductor.common.metadata.tasks.Task;
+import com.netflix.conductor.common.metadata.tasks.TaskResult;
+import org.springframework.stereotype.Component;
+
+@Component
+public class helloworld implements Worker {
+    @Override
+    public String getTaskDefName() {
+        return "hello_world";
+    }
+
+    @Override
+    public TaskResult execute(Task task) {
+        TaskResult result = new TaskResult(task);
+        result.addOutputData("hw_response", "Hello World!");
+        result.setStatus(TaskResult.Status.COMPLETED);
+        return result;
+    }
+}
