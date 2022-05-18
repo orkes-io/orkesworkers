af23c7b472bde1e9931b4c306e324eccfdd81514
Jeu George <jeu@orkes.io>
2022-05-18 00:27:28 -0700

    Bulk Image resize convert

--- src/main/java/io/orkes/samples/workers/ImageListMultipleConvertResizeWorker.java
+++ src/main/java/io/orkes/samples/workers/ImageListMultipleConvertResizeWorker.java
@@ -7,6 +7,8 @@ import com.google.common.collect.Maps;
 import com.netflix.conductor.client.worker.Worker;
 import com.netflix.conductor.common.metadata.tasks.Task;
 import com.netflix.conductor.common.metadata.tasks.TaskResult;
+import com.netflix.conductor.common.metadata.tasks.TaskType;
+import com.netflix.conductor.common.metadata.workflow.SubWorkflowParams;
 import com.netflix.conductor.common.metadata.workflow.WorkflowTask;
 import io.orkes.samples.utils.Size;
 import org.im4java.core.ConvertCmd;
@@ -15,19 +17,23 @@ import org.springframework.stereotype.Component;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
 @Component
-public class ImageMultipleConvertResizeWorker implements Worker {
+public class ImageListMultipleConvertResizeWorker implements Worker {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
 
     @Override
     public String getTaskDefName() {
-        return "image_multiple_convert_resize";
+        return "image_list_multiple_convert_resize";
     }
 
     @Override
@@ -36,35 +42,37 @@ public class ImageMultipleConvertResizeWorker implements Worker {
         TaskResult result = new TaskResult(task);
 
         try {
-            String fileLocation = (String) task.getInputData().get("fileLocation");
+            List<String> fileLocations = (List<String>) task.getInputData().get("fileLocations");
             List<String> outputFormats = (List<String>)(task.getInputData().get("outputFormats"));
             List<Size> outputSizes = (List<Size>) objectMapper.convertValue(task.getInputData().get("outputSizes"), new TypeReference<List<Size>>(){});
-            Boolean maintainAspectRatio = Boolean.valueOf(task.getInputData().get("maintainAspectRatio").toString());
+            Boolean maintainAspectRatio = Boolean.valueOf((String)task.getInputData().get("maintainAspectRatio"));
 
             List<WorkflowTask> dynamicTasks =  Lists.newArrayList();
             Map<String, Object> dynamicTasksInput = Maps.newHashMap();
 
             int i=0;
-            String dynamicTaskName = "image_convert_resize";
-            for (String outputFormat :
-                    outputFormats) {
-                for (Size size:
-                     outputSizes) {
-                    String taskRefName = String.format("%s_%s_%sx%s_%d",dynamicTaskName, outputFormat, size.width, size.height, i++);
+            String dynamicTaskName = "image_multiple_convert_resize";
+            for (String fileLocation :
+                    fileLocations) {
+
+                    String fileName = Paths.get(new URI(fileLocation).getPath()).getFileName().toString();
+                    String taskRefName = String.format("%s_%s_%d",dynamicTaskName, fileName, i++);
                     WorkflowTask dynamicTask = new WorkflowTask();
                     dynamicTask.setName(dynamicTaskName);
                     dynamicTask.setTaskReferenceName(taskRefName);
+                    dynamicTask.setWorkflowTaskType(TaskType.SUB_WORKFLOW);
+                    SubWorkflowParams subWorkflowParams = new SubWorkflowParams();
+                    subWorkflowParams.setName("image_multiple_convert_resize");
+                    dynamicTask.setSubWorkflowParam(subWorkflowParams);
                     dynamicTasks.add(dynamicTask);
 
                     Map<String, Object> dynamicTaskInput = Maps.newHashMap();
                     dynamicTaskInput.put("fileLocation", fileLocation);
-                    dynamicTaskInput.put("outputFormat", outputFormat);
-                    dynamicTaskInput.put("outputWidth", size.width);
-                    dynamicTaskInput.put("outputHeight", size.height);
+                    dynamicTaskInput.put("outputFormats", outputFormats);
+                    dynamicTaskInput.put("outputSizes", outputSizes);
                     dynamicTaskInput.put("maintainAspectRatio", maintainAspectRatio);
 
                     dynamicTasksInput.put(taskRefName,dynamicTaskInput );
-                }
             }
 
 
@@ -84,6 +92,5 @@ public class ImageMultipleConvertResizeWorker implements Worker {
         }
         return result;
     }
-
 
 }
