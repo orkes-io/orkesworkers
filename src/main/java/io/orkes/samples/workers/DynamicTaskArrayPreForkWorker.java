eb582b78508f18ad8c1e06f7503468c79edf4ee9
Jeu George <jeu@orkes.io>
2023-01-14 18:19:44 -0800

    Dynamic pre fork with arrays

--- src/main/java/io/orkes/samples/workers/DynamicTaskArrayPreForkWorker.java
+++ src/main/java/io/orkes/samples/workers/DynamicTaskArrayPreForkWorker.java
@@ -1,6 +1,5 @@
 package io.orkes.samples.workers;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -10,28 +9,24 @@ import com.netflix.conductor.common.metadata.tasks.TaskResult;
 import com.netflix.conductor.common.metadata.tasks.TaskType;
 import com.netflix.conductor.common.metadata.workflow.SubWorkflowParams;
 import com.netflix.conductor.common.metadata.workflow.WorkflowTask;
-import io.orkes.samples.utils.Size;
 import org.springframework.stereotype.Component;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.net.URI;
-import java.nio.file.Paths;
-import java.time.Instant;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 @Component
-public class DynamicTaskPreForkWorker implements Worker {
+public class DynamicTaskArrayPreForkWorker implements Worker {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
 
     @Override
     public String getTaskDefName() {
-        return "dynamic_task_prefork";
+        return "dynamic_task_array_prefork";
     }
 
     @Override
@@ -40,11 +35,17 @@ public class DynamicTaskPreForkWorker implements Worker {
 
         try {
 
-            List<Map<String, Object>> subWorkflowInputs = (List<Map<String, Object>>) task.getInputData().get("subWorkflowInputArray");
-            String subWorkflowName = (String)(task.getInputData().get("subWorkflowName"));
+            List<Map<String, Object>> inputs = (List<Map<String, Object>>) task.getInputData().get("inputsArray");
 
-            List<Map<String, Object>> taskInputs = (List<Map<String, Object>>) task.getInputData().get("taskInputs");
-            String taskName = (String)(task.getInputData().get("taskName"));
+            String subWorkflowName = null;
+            if(task.getInputData().containsKey("subWorkflowName")) {
+                subWorkflowName = (String)(task.getInputData().get("subWorkflowName"));
+            }
+
+            String taskName = null;
+            if(task.getInputData().containsKey("taskName")) {
+                taskName = (String) (task.getInputData().get("taskName"));
+            }
 
             String taskType = ((String) task.getInputData().get("taskType")).toLowerCase();
             TaskType type = validateTaskTypes(taskType);
@@ -54,27 +55,30 @@ public class DynamicTaskPreForkWorker implements Worker {
             Map<String, Object> dynamicTasksInput = Maps.newHashMap();
 
             int i=0;
-            String dynamicTaskName = subWorkflowName;
-            for (Map<String, Object> subWorkflowInput :
-                    subWorkflowInputs) {
+            String dynamicTaskName = (type == TaskType.SUB_WORKFLOW) ? subWorkflowName : taskName;
+            for (Map<String, Object> input :
+                    inputs) {
 
 
                 String taskRefName = String.format("%s_%d",dynamicTaskName, i++);
                 WorkflowTask dynamicTask = new WorkflowTask();
                 dynamicTask.setName(dynamicTaskName);
                 dynamicTask.setTaskReferenceName(taskRefName);
-                dynamicTask.setWorkflowTaskType(TaskType.SUB_WORKFLOW);
-                SubWorkflowParams subWorkflowParams = new SubWorkflowParams();
-                subWorkflowParams.setName(subWorkflowName);
-                dynamicTask.setSubWorkflowParam(subWorkflowParams);
+
+                if(type == TaskType.SUB_WORKFLOW) {
+                    dynamicTask.setWorkflowTaskType(TaskType.SUB_WORKFLOW);
+                    SubWorkflowParams subWorkflowParams = new SubWorkflowParams();
+                    subWorkflowParams.setName(subWorkflowName);
+                    dynamicTask.setSubWorkflowParam(subWorkflowParams);
+                }
+
                 dynamicTasks.add(dynamicTask);
                 Map<String, Object> dynamicTaskInput = Maps.newHashMap();
 
-                for (String taskInputKey:
-                        subWorkflowInput.keySet()) {
-                    dynamicTaskInput.put(taskInputKey, subWorkflowInput.get(taskInputKey));
+                for (String inputKey:
+                        input.keySet()) {
+                    dynamicTaskInput.put(inputKey, input.get(inputKey));
                 }
-
 
 
                 dynamicTasksInput.put(taskRefName,dynamicTaskInput);
